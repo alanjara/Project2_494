0dc0ddc1de26cfe27921dc03ecb6ae656f1c17dc
jordomck <jordomck@umich.edu>
2016-02-14 16:30:02 -0500

    Added Rewind Effect and Sound
    also, began a credits file.

---
+++ Assets/_Scripts/RewindEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RewindEffect : MonoBehaviour {
+    public MeshRenderer rend;
+    public AudioSource rewindSoundMaker;
+
+	// Use this for initialization
+	void Start () {
+	    rend = this.gameObject.GetComponent<MeshRenderer>();
+        rewindSoundMaker = this.gameObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rend.enabled = Camera.main.GetComponent<Main>().rewind;
+        rewindSoundMaker.mute = !Camera.main.GetComponent<Main>().rewind;
+	
+	}
+}
