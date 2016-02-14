51ea65db9151fe86e0e3b497301f41f76f089df8
jordomck <jordomck@umich.edu>
2016-02-14 18:03:32 -0500

    Added Thwomp enemy
    I didn't want to mess with player control, so for the moment the thwomp
    and spikes are only lethal to the clone.

---
+++ Assets/_Scripts/Spikes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spikes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("Hit a trigger with a thwomp");
+        if (other.gameObject.tag == "Player" && other.gameObject.name.Contains("Clone"))
+        {
+            Main.MCU.inUse = false;
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/_Scripts/Thwomp.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thwomp : MonoBehaviour {
+    public float fallspeed = 10f;
+    public float raisespeed = 2f;
+    public int stayLength = 20;
+    public float aggroWidth = 2f;
+    public float rayOffset = .9f;
+    public bool grounded;
+    int groundedCount;
+    Vector3[] Moments;
+    Vector3 startingPos;
+    Rigidbody rigid;
+    RigidbodyConstraints standardConstraints;
+    bool fall;
+
+	// Use this for initialization
+	void Start () {
+        fall = false;
+        
+        groundedCount = 0;
+        startingPos = transform.position;
+        Moments = new Vector3[2000];
+        Moments[0] = transform.position;
+        rigid = GetComponent<Rigidbody>();
+        standardConstraints = rigid.constraints;
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        if (collisionInfo.other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            grounded = true;
+    }
+
+    void reset()
+    {
+        transform.position = startingPos;
+        fall = false;
+    }
+
+    bool aggro()
+    {
+        bool triggered = false;
+        GameObject[] playerLikeObjects = GameObject.FindGameObjectsWithTag("Player");
+        foreach(GameObject player in playerLikeObjects)
+        {
+            Vector3 pos = player.transform.position;
+            Vector3 thwompPos = transform.position;
+            if (Mathf.Abs(thwompPos.x - pos.x) < aggroWidth && thwompPos.y > pos.y + 1f){
+                triggered = true;
+            }
+        }
+        return triggered;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Camera.main.GetComponent<Main>().rewind)
+        {
+            transform.position = Moments[Camera.main.GetComponent<Main>().currentFrame];
+            if(groundedCount > 0)
+            {
+                groundedCount -= 1;
+            }
+        }
+        else
+        {
+            Moments[Camera.main.GetComponent<Main>().currentFrame] = transform.position;
+            if (grounded)
+            {
+                groundedCount += 1;
+            }
+            if (groundedCount > stayLength)
+            {
+               groundedCount = 0;
+               fall = false;
+               grounded = false;
+            }
+            if (aggro())
+            {
+                fall = true;
+            }
+
+            if (fall)
+            {
+                rigid.constraints = standardConstraints;
+                rigid.velocity = Vector3.down * fallspeed;
+            }
+            else if (transform.position.y < startingPos.y)
+            {
+                rigid.constraints = standardConstraints;
+                rigid.velocity = Vector3.up * raisespeed;
+            }
+            else
+            {
+                rigid.constraints = RigidbodyConstraints.FreezeAll;
+                rigid.velocity = Vector3.zero;
+            }
+        }
+	
+	}
+}
